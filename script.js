c0a37e80e563120b8a8ce3f75f732e2dfea835dc
Nicolae1188 <32008196+Nicolae1188@users.noreply.github.com>
2019-06-13 17:16:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 //Dice images stored in an array
 diceImgs = [
-    "./img/dice1.png",
-    "./img/dice2.png",
-    "./img/dice3.png",
-    "./img/dice4.png",
-    "./img/dice5.png",
-    "./img/dice6.png"
+    "/img/dice1.png",
+    "/img/dice2.png",
+    "/img/dice3.png",
+    "/img/dice4.png",
+    "/img/dice5.png",
+    "/img/dice6.png"
 ]
 
 var score = 0;
@@ -28,13 +28,13 @@ function rollDice(){
     //Get dice images from array that equals with the random number
     img.src = diceImgs[randRoll];
     
-    //Condition roll 1 = gameover 21 = win
-       if (score == 1) {
-        alert ("Game Over");
+    //Condition roll 1 = gameover ,total score 21 = win
+    if (score == 1) {
+        alert ("Game Over, you've rolled 1.");
         window.location.reload();
         score = 0;
-    } else if (score > 22) {
-        alert  ("You've won");
+    } else if (score > 20) {
+        alert  ("Your score passed 20,You've won!");
         window.location.reload();
         score = 0;
     }
